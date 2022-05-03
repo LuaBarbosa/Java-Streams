789b18fe81d268ea92a5525547fb6eb6c0cbd161
LuaBarbosa <lua.barbosa@yahoo.com.br>
2022-05-02 21:54:45 -0300

    Feat: Criando classe e metodos para implementar pesquisas

--- src/Oscar/Oscar.java
+++ src/Oscar/Oscar.java
@@ -1,4 +1,27 @@
 package Oscar;
 
+import java.io.IOException;
+
 public class Oscar {
+
+
+    public static void main(String[] args) {
+       // atorMaisJovem();
+       // atrizMaispremiada();
+
+    }
+
+    public static void atorMaisJovem() throws IOException{
+        //Quem foi o ator mais jovem a ganhar um Oscar?
+    };
+
+    public static void atrizMaispremiada() throws IOException{
+        //Quem foi a atriz que mais vezes foi premiada?
+    }
+
+    public static void atrizEntreVinteETrintaMaisVencedora() throws IOException{
+        //Qual atriz entre 20 e 30 anos que mais vezes foi vencedora?
+    }
+
+
 }
