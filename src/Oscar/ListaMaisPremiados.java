5ddefcb226c3d03676425d7a9ce265fd30699fc0
LuaBarbosa <lua.barbosa@yahoo.com.br>
2022-05-02 21:53:18 -0300

    Feat: Criando classe para listar premiados

---
+++ src/Oscar/ListaMaisPremiados.java
@@ -0,0 +1,15 @@
+package Oscar;
+
+import java.io.IOException;
+
+public class ListaMaisPremiados {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void maisPremiado() throws IOException{
+        //Quais atores ou atrizes receberam mais de um Oscar?
+        // Elabore uma única estrutura contendo atores e atrizes.
+    }
+}
