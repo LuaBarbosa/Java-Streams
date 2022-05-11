6d5959cb65ed72322b5b7d1907f1ac350d5da3c6
LuaBarbosa <lua.barbosa@yahoo.com.br>
2022-05-11 09:21:11 -0300

    Feat: Lendo, unindo e criando nova lista

--- src/Oscar/Oscar.java
+++ src/Oscar/Oscar.java
@@ -4,53 +4,54 @@ package Oscar;
 import java.io.IOException;
 
 import java.nio.file.Files;
+
+
 import java.nio.file.Path;
 import java.nio.file.Paths;
-
-
-import java.util.Comparator;
+import java.util.ArrayList;
 import java.util.List;
-
-import java.util.Optional;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 
 public class Oscar {
 
     public static void main(String[] args) throws IOException {
-        atorMaisJovem();
+        List<Actors> ActorsList = readLines(Paths.get("Arquivo1.csv"));
+        List<Actors> ActressList = readLines(Paths.get("Arquivo2.csv"));
+
+        List<Actors> allActorList = new ArrayList<>(ActorsList.size() + ActressList.size());
+        allActorList.addAll(ActorsList);
+        allActorList.addAll(ActressList);
+
+
 
     }
 
-    public static void atorMaisJovem() throws IOException {
-        //Quem foi o ator mais jovem a ganhar um Oscar?
-        Path path = Paths.get("Arquivo1.csv");
-        Stream<String> stream = Files.lines(path);
-        List<Actors> listActors;
-        listActors = stream
+    public static List<Actors> readLines(Path file) throws IOException {
+    //criando uma lista para ler arquivo
+        List<String> lines = Files.readAllLines(file);
+        List<Actors> OscarList = new ArrayList<>();
+
+        lines.stream()
                 .skip(1)
                 .map(line -> line.split(";"))
-                .map(col-> new Actors(Integer.parseInt(col[0].trim()), Integer.parseInt(col[1].trim()), Integer.parseInt(col[2].trim()), col[3].trim(), col[4].trim()))
-               .collect(Collectors.toList());
-
-
-        System.out.println(listActors);
-
-        Optional<Integer> younger =
-        listActors.stream().min(Comparator.comparing(a -> a.getAge());
-    };
-
-    //public static void atrizMaispremiada() throws IOException{
-        //Quem foi a atriz que mais vezes foi premiada?
-
-        //Path path = Paths.get("Arquivo1.csv");
-       // Stream<String> stream = Files.lines(path);
-
-
+                .forEach(line -> {
+                    OscarList.add(new Actors(Integer.parseInt(line[0].trim()),
+                            Integer.parseInt(line[1].trim()), Integer.parseInt(line[2].trim()),
+                            line[3].trim(), line[4].trim()));
+                });
+        return OscarList;
     }
 
-    //public static void atrizEntreVinteETrintaMaisVencedora() throws IOException{
+
+        public static String youngActor (){
+
+
+
+        };
+}
+
+
+//public static void atrizEntreVinteETrintaMaisVencedora() throws IOException{
         //Qual atriz entre 20 e 30 anos que mais vezes foi vencedora?
 
        // Path path = Paths.get("Arquivo1.csv");
